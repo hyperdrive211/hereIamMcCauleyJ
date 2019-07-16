4097feb4baa1d524378cb9fb3a4e6b89bec7d0a1
Johnny <jmccauley117@gmail.com>
2019-07-16 22:03:49 +0100

    Begin Map Implementation
    Started bringing in the map implementation.

--- app/src/main/java/com/example/hereiammccauleyj/MainActivity.java
+++ app/src/main/java/com/example/hereiammccauleyj/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.hereiammccauleyj;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
-}
+
+
+    }
+
--- app/src/main/java/com/example/hereiammccauleyj/Splash.java
+++ app/src/main/java/com/example/hereiammccauleyj/Splash.java
@@ -2,14 +2,24 @@ package com.example.hereiammccauleyj;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 
 public class Splash extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
+    private static final int ERROR_DIALOG_REQUEST = 9001;
     private ImageView iv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,22 +29,51 @@ public class Splash extends AppCompatActivity {
         Animation myAnim = AnimationUtils.loadAnimation(
                 this,R.anim.mytransition);
         iv.startAnimation(myAnim);
-        final Intent i = new Intent(this, MainActivity.class);
+       if(isServicesOK()) {
+           Thread timer = new Thread() {
+               public void run() {
+                   try {
+                       sleep(5000);
+                   } catch (InterruptedException e) {
+                       e.printStackTrace();
+                   } finally {
+                       finish();
+                       init();
+                   }
+               }
+           };
 
-        Thread timer = new Thread(){
-            public void run(){
-                try{
-                   sleep(5000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                finally {
-                   startActivity(i);
-                   finish();
-                }
-            }
-        };
+           timer.start();
+       }
+    }
 
-        timer.start();
+    public boolean isServicesOK(){
+        Log.d(TAG, "isServicesOK: checking google services version");
+
+        int available = GoogleApiAvailability
+                .getInstance()
+                .isGooglePlayServicesAvailable(Splash.this);
+
+        if(available == ConnectionResult.SUCCESS){
+            //Everything is fine and the user can make map requests
+            Log.d(TAG, "isServicesOK: Google PLay Services is working");
+            return true;
+        }else if(GoogleApiAvailability.getInstance().isUserResolvableError(available)){
+            //An error has occured and we can resolve it
+            Log.d(TAG, "isServicesOK: an error occured but we can fix it");
+            Dialog dialog = GoogleApiAvailability
+                    .getInstance()
+                    .getErrorDialog(Splash.this, available, ERROR_DIALOG_REQUEST);
+            dialog.show();
+        }
+        else {
+            Toast.makeText(this, "We can't make map requests", Toast.LENGTH_LONG).show();
+        }
+        return false;
+    }
+
+    private void init() {
+     Intent intent = new Intent(Splash.this, MainActivity.class);
+     startActivity(intent);
     }
 }
