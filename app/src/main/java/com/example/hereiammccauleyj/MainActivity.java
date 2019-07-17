3394708060eb61a7b3c93b62d263048f61f52d92
Johnny <jmccauley117@gmail.com>
2019-07-17 23:45:44 +0100

    Map initialisation
    More work on Map

--- app/src/main/java/com/example/hereiammccauleyj/MainActivity.java
+++ app/src/main/java/com/example/hereiammccauleyj/MainActivity.java
@@ -1,9 +1,15 @@
 package com.example.hereiammccauleyj;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.Activity;
 import android.app.Dialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -12,19 +18,86 @@ import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback{
+
+
 
     private static final String TAG = "MainActivity";
+    private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
+    private static final String COARSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
+
+    //variables
+    private Boolean mLocationPermissionGranted = false;
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        getLocationPermission();
+    }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        Toast.makeText(this, "Map is ready" ,Toast.LENGTH_SHORT).show();
+        mMap = googleMap;
+    }
+
+    private void initMap(){
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+
+        mapFragment.getMapAsync(MainActivity.this);
+    }
+
+    private void getLocationPermission(){
+        String [] permissions = {FINE_LOCATION, COARSE_LOCATION};
+
+        if(ContextCompat.checkSelfPermission(this.getApplicationContext(),FINE_LOCATION)
+        == PackageManager.PERMISSION_GRANTED){
+            if(ContextCompat.checkSelfPermission(this.getApplicationContext(),COARSE_LOCATION)
+                    == PackageManager.PERMISSION_GRANTED){
+              mLocationPermissionGranted = true;
+              initMap();
+            }else {
+                ActivityCompat.requestPermissions(this,
+                        permissions,
+                        LOCATION_PERMISSION_REQUEST_CODE);
+            }
+        } else{
+            ActivityCompat.requestPermissions(this,
+                    permissions,
+                    LOCATION_PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+       mLocationPermissionGranted = false;
+
+       switch(requestCode){
+           case LOCATION_PERMISSION_REQUEST_CODE : {
+               if(grantResults.length > 0){
+                 for(int i = 0; i < grantResults.length; i++){
+                     if(grantResults[i] != PackageManager.PERMISSION_GRANTED){
+                         mLocationPermissionGranted = false;
+                         return;
+                     }
+                 }
+                 mLocationPermissionGranted = true;
+                 //initialise our map
+                 initMap();
+               }
+           }
+       }
     }
 
 
-    }
+}
 
